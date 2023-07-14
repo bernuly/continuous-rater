7a885483f72a5c297f9f8b3d2790d4544487ae67
bernuly <bernuly@gmail.com>
2023-07-14 22:30:52 +0100

    nr 5

--- src/utils.js
+++ src/utils.js
@@ -37,7 +37,7 @@ export const totalHITTime = estHITTime * 2; // total time provided for HIT (in m
 
 // stimuli variables      
 // export const ratingTypes = ['Anger', 'Joy', 'Sadness', 'Suprise', 'Fear', 'Shame']; // array of rating types   
-export const ratingTypes = ['Anger']; // array of rating types   
+export const ratingTypes = ['Anger', 'Sadness']; // array of rating types   
 
 // this configures path to proper firebase
 // COPY AND PASTE YOUR FIREBASE CONFIG HERE
